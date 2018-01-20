6d55ee3f0dc4415c89099b556f6cb62475caf370
Souradip Mookerjee <sm2030@cam.ac.uk>
2018-01-20 18:54:04 +0000

    Fix bugs

--- run.sh
+++ run.sh
@@ -4,8 +4,15 @@ if [ $? -eq 1 ]; then
     ipfs init
     ipfs daemon &
 fi
+pgrep flask
+if [ $? -eq 1 ]; then
 cd back_end
-back_end/run.sh &
+./run.sh &
 cd ..
+fi
+
+pgrep "^ng"
+if [ $? -eq 1 ]; then
 cd front_end
 ng serve --host 0.0.0.0
+fi
