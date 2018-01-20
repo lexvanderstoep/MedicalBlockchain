ee35a6bc5998686010b539ab9163e18ac4476ee7
Souradip Mookerjee <sm2030@cam.ac.uk>
2018-01-20 18:49:12 +0000

    Added more ipfs error checking

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-ipfs init
-ipfs daemon &
+pgrep ipfs
+if [ $? -eq 1 ]; then
+    ipfs init
+    ipfs daemon &
+fi
+cd back_end
 back_end/run.sh &
+cd ..
 cd front_end
 ng serve --host 0.0.0.0
