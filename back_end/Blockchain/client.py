f8242c38ce67de7bb3680bf390882cc1e84c7e0b
Lex van der Stoep <lex.vanderstoep@gmail.com>
2018-01-21 01:54:46 +0000

    Half-done client

--- back_end/Blockchain/client.py
+++ back_end/Blockchain/client.py
@@ -1,4 +1,5 @@
 import arky.rest
+import requests
 arky.rest.use("dark")
 
 thisSecret = "gadget shoulder napkin betray argue any title nice gadget divert slam involve"
@@ -10,15 +11,29 @@ print("Secret: " + thisSecret)
 print("PK: " + thisPK)
 print("Address: " + thisAddress)
 
+'Get records in sets of 50 in JSON format'
+def getTransactions(offset):
+    headers = {
+        'accept': 'application/json',
+        'nethash': '6e84d08bd299ed97c212c886c98a57e36545c8f5ca7eeae63a8bd62d8988',
+        'version': '1.0.1',
+        'port': '4001',
+    }
+    params = (
+        ('offset', '0'),
+        ('vendorField', 'Thank you for voting.'),
+    )
+    response = requests.get('http://138.197.165.165:4002/api/transactions', headers=headers, params=params)
+    return response.text
+
 def getLatestHashCode(ID):
-    'TODO: implement this properly'
-    if (ID == "Lex van der Stoep"):
-        return "Lex' hash"
-    else:
-        return ""
+    'Get the transactions in sets of 50 in json format.'
+    for i in range(1,10):
+        getTransactions(i*50)
 
 def askOwnership(ID):
     'implement this'
+    return 0
 
 def giveOwnership(ownSecret, recipientAddress, ID):
     'Get the latest hash code belonging to ID'
@@ -36,4 +51,7 @@ def secretToAddress(NHSID):
     address = arky.core.crypto.getAddress(publicKey)
     return address
 
-print(giveOwnership(thisSecret, secretToAddress("mule pupil peace among doll spot half define dizzy pole village beef"), "Lex van der Stoep"))
+'print(giveOwnership(thisSecret, secretToAddress("mule pupil peace among doll spot half define dizzy pole village beef"), "Lex van der Stoep"))'
+print(getTransactions(0))
+getLatestHashCode(0)
+print("done")
